5cd2dfada6240c523e6dc806931be7e144ac41f6
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-16 16:10:38 +0600

    fixed margin

--- src/Components/Hero2.js
+++ src/Components/Hero2.js
@@ -7,8 +7,8 @@ const Hero2 = () => {
             <img src={avatar} alt="" className='rounded-full w-24 shadow-lg mx-auto' />
             <div className=''>
                 <div className='w-8/12 mx-auto'>
-                    <img src={mark} alt="" width={65} className="" />
-                    <p className='text-4xl  font-[Inter] text-[#323639] text-center'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi atque doloremque beatae! Doloremque perspiciatis aliquid repellat quasi praesentium, minima nobis assumenda ex?</p>
+                    <img src={mark} alt="" width={65} className="-mx-16" />
+                    <p className='text-4xl  font-[Inter] text-[#323639] text-center'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi atque doloremque beatae! Doloremque perspiciatis aliquid repellat quasi praesentium, minima nobis assumenda ex?</p> 
                 </div>
                 <div className='my-12 text-center'>
                     <p className='font-bold text-xl text-[#323639]'>EMILY EWING</p>
